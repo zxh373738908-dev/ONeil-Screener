c3887a2f3868a4903cbb4cbf813dea11865cbcf4
zxh373738908-dev <zxh373738908@gmail.com>
2026-03-12 14:35:43 +0800

    Update run_a.py

--- run_a.py
+++ run_a.py
@@ -400,4 +400,9 @@ def write_to_sheet(sheet_name, final_stocks, sort_col, diag_msg=None):
 
 if __name__ == "__main__":
     try:
-        res, ms
+        res, msg = screen_a_shares()
+        write_to_sheet("A-Share Screener", res, "60D_Return%", diag_msg=msg)
+    except Exception as e:
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        error_info = traceback.format_exc()
+        write_to_sheet("A-Share Screener",[], "60D_Return%", diag_msg=f"[{now}] 致命崩溃:\n{error_info}")
